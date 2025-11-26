331a99fbd9400dfe92ce4c126fac96250655d8c1
Spencer Parkin <spencer.parkin@proton.me>
2025-11-25 23:12:20 -0700

    Add test of removal from binomial heap.  Not yet passing.

--- Test/Source/BinomialHeapTest.cpp
+++ Test/Source/BinomialHeapTest.cpp
@@ -21,4 +21,24 @@ TEST_CASE("BinomialHeaps", "[binomial_heap]")
 			REQUIRE(validHeap);
 		}
 	}
+
+	SECTION("Removal.")
+	{
+		BinomialHeap heap;
+
+		for (int i = 0; i < 100; i++)
+		{
+			int j = random.GetRandomInteger(0, 100);
+			heap.Insert(new BinomialHeap::TypedNode<int>(j));
+		}
+
+		int lastKey = 200;
+		for (int i = 0; i < 100; i++)
+		{
+			auto node = static_cast<BinomialHeap::TypedNode<int>*>(heap.RemoveMinimal());
+			REQUIRE(node != nullptr);
+			REQUIRE(node->key <= lastKey);
+			lastKey = node->key;
+		}
+	}
 }
